bb538ab2ec1569ebd5c7eb4fa3c938e38f440220
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-03-10 16:36:48 +0530

    Fixed: Code Generation

--- Picto Pursuit/JoinRoom.swift
+++ Picto Pursuit/JoinRoom.swift
@@ -8,18 +8,28 @@
 import SwiftUI
 
 struct JoinRoom: View {
-    @State private var code: String = ""
-    
+    @State private var code: Int?
+    @State private var inputCode: String = ""
+    @State private var isDrawingBoardActive = false
     
     var body: some View {
         VStack {
-            TextField("Enter Room Code", text: $code)
+            TextField("Enter Room Code", text: $inputCode)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
+            NavigationLink(
+                destination: DrawingBoardText(roomCode: code ?? 0),
+                isActive: $isDrawingBoardActive) {
+                EmptyView()
+            }
+            
             Button(action: {
                 
-                
+                if let roomCode = Int(inputCode) {
+                    code = roomCode
+                    isDrawingBoardActive = true
+                }
             }) {
                 Text("Submit")
                     .padding()
